92ce3a96b688021a5881d29c2381002d50b690ca
MischaTobias <mischa.tobias4b@gmail.com>
2020-02-27 00:35:30 -0600

    Binary Tree AddingFunc, done

---
+++ CustomGenerics/Structures/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CustomGenerics.Interfaces;
+using CustomGenerics.Structures;
+
+namespace CustomGenerics.Structures
+{
+    public class BinaryTree<T> : DataStructureBase<T>
+    {
+        private BinaryTreeNode<T> root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void AddMedicine(T medicine, T docLine, Comparison<BinaryTreeNode<T>> Comparison)
+        {
+            BinaryTreeNode<T> node = new BinaryTreeNode<T> { medicine = medicine, docLine = docLine, leftSon = null, rightSon = null };
+            Insert(root, node, Comparison);
+        }
+
+        protected override void Insert(BinaryTreeNode<T> currentNode, BinaryTreeNode<T> newNode, Comparison<BinaryTreeNode<T>> Comparison)
+        {
+            if (currentNode == null)
+            {
+                currentNode = newNode;
+            }
+            else if (Comparison(currentNode, newNode) < 0)
+            {
+                currentNode = currentNode.leftSon;
+                Insert(currentNode, newNode, Comparison);
+            }
+            else
+            {
+                currentNode = currentNode.rightSon;
+                Insert(currentNode, newNode, Comparison);
+            }
+        }
+
+
+        protected override void Delete(BinaryTreeNode<T> currentNode, T value, Comparison<T> comparer)
+        {
+
+        }
+
+        protected override T GetT()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
